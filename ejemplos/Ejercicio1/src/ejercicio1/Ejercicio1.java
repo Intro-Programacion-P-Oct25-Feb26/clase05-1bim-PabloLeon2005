fe7fa4915897fc339c024e767e98423e0beb6ffa
PabloLeon2005 <pabloguaman2005@gmail.com>
2025-11-12 23:52:23 -0500

    EJERCICIO DEBER

---
+++ ejemplos/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner entrada = new Scanner(System.in);
+    entrada.useLocale(Locale.US);    
+    
+            String nombre;
+        String apellido;
+        String usuario;
+        int edad;
+        String parroquia;
+        int diaPago;
+        int mensualidad = 25;
+        double totalDescuento;
+        double total;
+        System.out.println("Ingrese su nombre por favor");
+        nombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido por favor");
+        apellido = entrada.nextLine();
+        System.out.println("Ingrese su parroquia por favor");
+        parroquia = entrada.nextLine();
+        System.out.println("Ingrese su Usuario por favor");
+        usuario = entrada.nextLine();
+        System.out.println("Ingrese su edad por favor");
+        edad = entrada.nextInt();
+        System.out.println("Ingrese su dia de pago por favor");
+        diaPago = entrada.nextInt();
+  
+        if (diaPago<10 && (parroquia.equals("El Valle") || 
+                parroquia.equals("El Sagrario")))
+        {
+            
+            totalDescuento = (mensualidad * .05);
+            total = (mensualidad-totalDescuento);
+            System.out.printf("Nombre: %s\nApellido: %s\nUsuario: %s\nEdad: "
+                    + "%s\nParroquia: %s\nDia de pago: %s\nMensualidad: $%s\n"
+                    + "Descuento: $%s\nTotal a Pagar: $%s\n",
+                    nombre, 
+                    apellido,
+                    usuario,
+                    edad, 
+                    parroquia, 
+                    diaPago, 
+                    mensualidad, 
+                    totalDescuento, 
+                    total);
+        }
+        else {
+            System.out.printf("Nombre: %s\nApellido: %s\nUsuario: %s\nEdad: "
+                    + "%s\nParroquia: %s\nDia de pago: %s\nMensualidad: $%s\n"
+                    + "Descuento: $0\nTotal a Pagar: $%s\n",
+                    nombre, 
+                    apellido, 
+                    usuario,
+                    edad, 
+                    parroquia, 
+                    diaPago, 
+                    mensualidad, 
+                    mensualidad);
+        }
+
+    }
+    
+}
